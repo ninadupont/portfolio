5ffb0e079d6ad0f94b9e85bb8ca076e12027c28b
ninadupont <ninadupont@users.noreply.github.com>
2018-02-22 22:35:18 +0100

    basics

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Nina Lehtinen - A multimedia design student with an urge to create</title>
+  <link rel="stylesheet" href="../build/css/reset.css">
+  <link rel="stylesheet" href="../build/css/layout.css">
+  <link rel="stylesheet" href="../build/css/_global.css">
+  <link rel="stylesheet" href="../build/css/styles.css">
+  <script defer src="../src/js/fontawesome-all.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Dosis|Sacramento" rel="stylesheet">
+</head>
+<body>
+  <header>
+        <ul class="soMe">
+          <li><i class="fab fa-instagram icon"></i></li>
+          <li><i class="fab fa-facebook-f icon"></i></li>
+        </ul>
+  </header>
+
+<section class="welcome">
+  <h1 class="welcome__hola">!Hola!</h1>
+  <div class="welcome__subTitle">
+    <p>I'm Nina, and this is going to be my awesome portfolio.</p>
+    <p>Yiihaaa</p>
+  </div>
+</section>
+
+<section class="about">
+  <h1 class="about__hola">!Hola!</h1>
+  <div class="about__subTitle">
+    <p>I'm Nina, and this is going to be my awesome portfolio.</p>
+    <p>Yiihaaa</p>
+  </div>
+</section>
+<section class="work">
+  </section>
+  <footer>
+  </footer>
+
+
+
+</body>
+</html>
+
+<link rel="stylesheet" href="">
